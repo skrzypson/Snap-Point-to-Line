11b3fba4dfb8ef7f3469fa71ddf3ae3283133e52
skrzypson <krzys.skrzyp@gmail.com>
2017-05-07 15:15:57 +0200

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -3,7 +3,6 @@ import java.awt.EventQueue;
 import java.awt.Graphics;
 import javax.swing.JFrame;
 
-
 public class Tester extends JFrame{
 	
 	private static void GUIinit(){
@@ -11,14 +10,14 @@ public class Tester extends JFrame{
 		EventQueue.invokeLater(new Runnable() {
 	        @Override
 	        public void run() {
-	            Tester ex = new Tester();
-	            ex.setSize(500,500);
-	            ex.setTitle("Position tracker");
-	            ex.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			Tester ex = new Tester();
+			ex.setSize(500,500);
+			ex.setTitle("Position tracker");
+			ex.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	    		ex.setLocationRelativeTo(null);
-	    		ex.setBackground(Color.WHITE);
-	            ex.setVisible(true);
-	            ex.add(new Tester_board());
+			ex.setBackground(Color.WHITE);
+			ex.setVisible(true);
+			ex.add(new Tester_board());
 	            
 	        }
 	    });
@@ -29,8 +28,6 @@ public class Tester extends JFrame{
 	    
 	    GUIinit();
 	    Thread connector = new Thread(new DB_connector()); 	// database connection object
-	    connector.start();									// starts connection
+	    connector.start();					// starts connection
 	}
-
-	
 }
