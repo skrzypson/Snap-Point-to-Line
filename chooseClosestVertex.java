643fb46d65af0cb32665cc09cc2821ff2aa701ec
skrzypson <krzys.skrzyp@gmail.com>
2017-05-07 15:18:55 +0200

    Update chooseClosestVertex.java

--- chooseClosestVertex.java
+++ chooseClosestVertex.java
@@ -3,15 +3,13 @@ public class chooseClosestVertex {
 	
 	public static int choose(int a, int[][] A, int p){
     	if (p == 0){
-			a = A[p][0];
-			System.out.println("a: " + a);
-		}
-		else if (p != 0){
-			a = A[p -1][1];
-			System.out.println("a: " + a);
-		}
+		a = A[p][0];
+		System.out.println("a: " + a);
+	}
+	else if (p != 0){
+		a = A[p -1][1];
+		System.out.println("a: " + a);
+	}
     	return a;
-    	
     }
-	
 }
